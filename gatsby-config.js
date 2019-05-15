befc374eaf0fd461b3a0067d8e54071b2b18048b
Nathanael Utama <nathan.utama@gmail.com>
2019-05-15 11:17:09 +1000

    Initial test with third-party GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,22 @@
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+
+// console.log(`Using environment config: '${activeEnv}'`)
+// const fs = require('fs');
+// const { buildSchema, buildClientSchema } = require('graphql');
+
+require('dotenv').config({
+  path: '.env',
+});
+
+// console.log(`Using environment config: '${ process.env.GRAPHQL_URL }'`)
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    apiUrl: process.env.GRAPHQL_URL,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +25,30 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    // Simple config, passing URL
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        // // This type will contain remote schema Query type
+        // typeName: 'SWAPI',
+        // // This is field under which it's accessible
+        // fieldName: 'swapi',
+        // // Url to query from
+        // url: 'https://api.graphcms.com/simple/v1/swapi',
+
+        fieldName: `cms`,
+        url: `https://api-euwest.graphcms.com/v1/cjjr1at6d0xb801c3scjrm0l0/master`,
+        typeName: `GraphCMS`,
+        refetchInterval: 60,
+
+        // createSchema: async () => {
+        //     const json = JSON.parse(
+        //       fs.readFileSync(`${__dirname}/introspection.json`)
+        //     )
+        //     return buildClientSchema(json.data)
+        // },
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,41 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
-import { Link } from "gatsby"
+import { makeBlogPath } from "../utils"
+import dateformat from "dateformat"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = ({ data }) => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>My Gatsby Blog</h1>
+    <p>
+      <a href="https://www.gatsbyjs.org/packages/gatsby-source-graphql/">
+        Using gatsby-source-graphql
+      </a>
+    </p>
+    {data.cms.blogPosts.map((blog, i) => (
+      <Link key={i} to={makeBlogPath(blog)}>
+        <h2>
+          {dateformat(blog.createdAt, `fullDate`)} - {blog.title}
+        </h2>
+      </Link>
+    ))}
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
 export default SecondPage
+
+export const query = graphql`
+  query {
+    cms {
+      blogPosts(where: { status: PUBLISHED }, orderBy: createdAt_DESC) {
+        title
+        createdAt
+        slug
+      }
+    }
+  }
+`
