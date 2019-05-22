1ea2521d23bf5e408f67e65b884e177e8bc129d3
Nathanael Utama <nathan.utama@gmail.com>
2019-05-22 16:32:27 +1000

    Update utility functions
    *mainly to prepare path and GraphQL result wrapper

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,32 @@
-const dateformat = require(`dateformat`)
 
-exports.buildPath = ({ title }) => {
-  // const date = new Date(createdAt)
-  // const formattedDate = dateformat(date, `yyyy-mm-dd`)
 
+// remove trailing slashes unless it's only "/"
+const replaceSlash = _path => ( _path === `/` ? _path : _path.replace(/\/$/, `` ))
+// remove slashes at the begining and end
+const replaceBothSlash = _path => _path.replace(/^\/|\/$/g, '')
+
+// for setting page name
+const setPageName = _path => {
+  const _name = replaceBothSlash( _path )
+  return _name === '' ? 'index' : _name
+}
+
+// check result.errors because graphql doesn't throw an error
+const GQLwrapper = promise =>
+  promise.then( result => {
+    if ( result.errors ) {
+      throw result.errors
+    }
+    return result
+  })
+
+// not used just yet
+const buildPath = ({ title }) => {
   // remove special chars
   let cleantitle = title.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '')
   // change hyphen for space
   let slug = cleantitle.replace(/\s+/g, '-')
   return `/${slug}`
 }
+
+module.exports = { replaceSlash, replaceBothSlash, setPageName, GQLwrapper }
