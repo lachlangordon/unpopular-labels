fc1bf2598567ad3302c6e55efda54743e31d02b9
Nathanael Utama <nathan.utama@gmail.com>
2019-07-02 17:17:41 +1000

    Create query resolvers
    - create narrative queries
    - create query to retrieve setobjects & images by parentId

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ const path = require(`path`)
 const crypto = require('crypto')
 
 // gatsby-node.js
-const { GraphQLSchema, GraphQLString } = require(`graphql`)
+const { GraphQLSchema, GraphQLString, GraphQLInt, GraphQLList } = require(`graphql`)
 const { createNarratives } = require('./src/lib/pageCreator')
 const { GatsbyNodeQuery, GatsbyAllNarrativeQuery } = require('./src/queries/ServerQuery')
 const { GQLGatsbyWrapper, GQLClientWrapper, printGraphQLError } = require(`./src/lib/graphQL`)
@@ -204,22 +204,84 @@ exports.createResolvers = ({
   reporter,
 }) => {
 
+  // // Construct a schema, using GraphQL schema language
+  // var schema = buildSchema(`
+  //   type Query {
+  //     SetObjectById( id: String! ): [NarrativeObject]
+  //   }
+  // `);
+  //
+  // console.log(schema)
+
   createResolvers({
     // Create a new root query field.
+    // Field resolvers can use all of Gatsby's querying capabilities
     Query: {
-      // Field resolvers can use all of Gatsby's querying capabilities
+      getMasterSetId: {
+        type: `String`,
+        resolve(source, args, context, info) {
+          return `${ __MASTER_NARRATIVE }`
+        }
+      },
+      getMasterSet: {
+        type: `Narrative`,
+        resolve(source, args, context, info) {
+          return context.nodeModel.getNodeById({
+              id: `${ __MASTER_NARRATIVE }`,
+              type: `Narrative`,
+            })
+        }
+      },
       SetsByMasterId: {
         type: [`Narrative`],
         resolve(source, args, context, info) {
-          console.log('inside create resolvers')
-          console.log(args)
+          // console.log('inside create resolvers')
+          // console.log(args)
           return context.nodeModel.runQuery({
-            query: { filter: { parent: { id: { eq:  `${ __MASTER_NARRATIVE }` } } } },
+            query: {
+              filter: { parent: { id: { eq:  `${ __MASTER_NARRATIVE }` } } },
+              sort: { fields: ['id'], order: ['ASC'] }
+            },
             type: `Narrative`,
             firstOnly: false,
           })
         }
-      }
+      },
+      SetObjectsByParentId: {
+        type: [`NarrativeObject`],
+        args: {
+          parentId: {
+            name: `parentId`,
+            type: GraphQLString,
+          },
+        },
+        resolve(source, args, context, info) {
+          // console.log(args)
+          const setObjects = context.nodeModel.getAllNodes({
+              type: `NarrativeObject`,
+            })
+
+          return setObjects.filter(setObj => setObj.parent == `${ args.parentId }`)
+        }
+      },
+      ImagesByParentId: {
+        type: [`Image`],
+        args: {
+          parentId: {
+            name: `parentId`,
+            type: GraphQLString,
+          },
+        },
+        resolve (source, args, context, info) {
+          // console.log("context - path")
+          // console.log(context.path)
+          const images = context.nodeModel.getAllNodes({
+              type: `Image`,
+            })
+
+          return images.filter(img => img.parent == `${ args.parentId }`)
+        }
+      },
     }
   })
 }
