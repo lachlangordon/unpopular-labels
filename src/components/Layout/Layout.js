13adb8a61bfa190d1fc0e0d48ab025c9c5971ff6
lachlangordan <lachlangordan@PH09875.local>
2019-10-09 17:15:31 +1100

    remove the kiosk mode force that snuck in

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -58,7 +58,6 @@ class Layout extends Component {
     // }
 
     render() {
-      window.source = "kiosk";
       const { location, children } = this.props;
       const appClassName = 'guide-app';
 
