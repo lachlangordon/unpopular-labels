46aa3e7f043cc42d3c09d8fca81b5d11310368b3
lachlangordan <lachlangordan@PH09875.local>
2019-10-09 11:49:01 +1100

    trying to style this cultural statement

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -7,13 +7,6 @@ import {shouldShowSeenIcon} from '../../lib/session';
 const Footer = (props) => (
     <footer className="footer">
         <section className="section footer-content">
-                {/*
-                  <h2>About the exhibition</h2>
-                  <p>{props.description}</p>
-                  <div className="logo-holder">
-                    <img src={maasLogo}/>
-                  </div>
-                */}
                 <div className="footer-content__acknowledgment">
                     <h3 className="footer-content__acknowledgment-title">Acknowledgement of Country</h3>
                     <p>The Museum of Applied Arts and Sciences acknowledges Australia’s First Nations Peoples as the Traditional Owners and Custodians of the land and gives respect to the Elders – past and present – and through them to all Aboriginal and Torres Strait Islander peoples.</p>
@@ -26,7 +19,6 @@ const Footer = (props) => (
                         {shouldShowSeenIcon() && (
                           <li className="menu-item"><Link to="/privacy">Privacy</Link></li>
                         )}
-                        {/* <li ><a href="https://www.nsw.gov.au/">NSW Government</a></li> */}
                       </ul>
                   </div>
                 </nav>
