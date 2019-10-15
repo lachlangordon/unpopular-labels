d7b78ef36204f8718ada961daa80223b09dbaba7
lachlangordan <lachlangordan@PH09875.local>
2019-10-15 17:37:28 +1100

    new creds and acks

--- src/pages/acknowledgements.js
+++ src/pages/acknowledgements.js
@@ -21,7 +21,7 @@ const AcknowledgementsPage = ({
               </h1>
 
               <div className="normal-page__description">
-                <p>The Powerhouse Museum warmly thank Jenny Kee and Linda Jackson for their generous collaboration in the creation of Step into Paradise. </p>
+                <p>The Powerhouse Museum warmly thank Jenny Kee and Linda Jackson for their generous collaboration in the creation of <cite>Step into Paradise</cite>. </p>
 
                 <h3><strong>Creative Director</strong></h3>
                 <p>Tony Assness</p>
@@ -32,22 +32,29 @@ const AcknowledgementsPage = ({
                 <h3><strong>Exhibition Designer</strong></h3>
                 <p>Jemima Woo</p>
 
+                <h3><strong>Music Curation and Composition</strong></h3>
+                <p>Jonny Seymour, Nick Wales</p>
+
+                <h3><strong>Lighting Design</strong></h3>
+                <p>Damien Cooper</p>
+
+                <h3><strong>Lighting</strong></h3>
+                <p>Peter Harmon</p>
+
                 <h3><strong>Project Coordinator</strong></h3>
                 <p>Kate Ford</p>
 
                 <h3><strong>Exhibition Design and Production</strong></h3>
-                <p>Angus Meredith, Arabella Cumyn-Jones, Buster Kruger, Chad Shah, Chloe Simcox, Cristina Briones, Damien Cooper, David Statham, Eloise Lindeback, Gavin Lewis, Iain Cooper, Jack Dunbar, Jessica Fitzpatrick, Joanne Delzoppo, Jo Lyons, Judith Matheson, Karen Hancock, Lauren Volk, Maria Mosquera, Pablo Donnan, Penny Angrick, Peter Hermon, Rose McEwen, Shane Fitzgerald, Stella Palmer, Stephen Chaumont, Trudi Fletcher, Vania Contreras, Victoria Nelson</p>
+                <p>Angus Meredith, Arabella Cumyn-Jones, Buster Kruger, Chad Shah, Chloe Simcox, Col Johnston, Cristina Briones, Dave Teer, David Statham, Eloise Lindeback, Gavin Lewis, Gill Scott, Iain Cooper, Jack Dunbar, Jessica Fitzpatrick, Joanne Delzoppo, Jo Lyons, Jo Westbury, Judith Matheson, Karen Hancock, Lauren Volk, Maria Mosquera, Neil Burtt, Pablo Donnan, Penny Angrick, Rocket Mattler, Rose McEwen, Stella Palmer, Stephen Chaumont, Trudi Fletcher, Vania Contreras, Victoria Nelson</p>
 
                 <h3><strong>Indigenous Engagement</strong></h3>
                 <p>Marcus Hughes</p>
 
                 <h3><strong>Registration and Conservation</strong></h3>
-                <p>Lucy Clark, Madeleine Riley, Sarah Heenan, Sarah Pointon, Skye Mitchell, Suzanne Chee, Teresa Werstak</p>
+                <p>Caitlin Lawler, Chris Redman, Lucy Clark, Madeleine Riley, Sarah Heenan, Sarah Pointon, Skye Mitchell, Suzanne Chee, Teresa Werstak, Tim Morris</p>
 
                 <h3><strong>Audio, Visual and Digital Media</strong></h3>
-                <p>Anna Fraser, Arul Baskaran, Ben Rumble, Brett Tweedie, David Cochrane, Gara Baldwin, Geoff Drane, Georgina Veneziani, Harry Ree, Jessica James-Moody, Julien Burgess, Karen Johnson, Kathleen Hackett, Kathleen Phillips, Lachlan Gordon, Marinco Kojdanovski, Michael Saad, Nathan Utama, Neil Burtt, Owen Conlan, Rebecca Godfrey, Ryan Hernandez, Stephen Mason, Tim Wilson</p>
-
-                <br />
+                <p>Anna Fraser, Arul Baskaran, Ben Rumble, Brett Tweedie, David Cochrane, Gara Baldwin, Geoff Drane, Georgina Veneziani, Harry Ree, Jessica James-Moody, Julian Burgess, Karen Johnson, Kathleen Hackett, Kathleen Phillips, Lachlan Gordon, Marinco Kojdanovski, Michael Saad, Nathan Utama, Owen Conlan, Rebecca Godfrey, Ryan Hernandez, Stephen Mason, Tim Wilson</p>
 
                 <h3><strong>Communications and Development</strong></h3>
                 <p>Alison Wares, Daniel McCready, Elizabeth Smith, Georgia Sholl, Kathleen Evesson, Kym Elphinstone, Mark Sieckman, Rachida Pearce, Sasha Haughan</p>
@@ -58,7 +65,9 @@ const AcknowledgementsPage = ({
                 <h3><strong>Visitor Services and Volunteers</strong></h3>
                 <p>Cate Purcell, Eloise Eaton, Jo Jewitt</p>
 
-                <p>We thank the following people and organisations who have generously supported Step into Paradise.</p>
+                <br/>
+
+                  <p>We thank the following people and organisations who have generously supported <cite>Step into Paradise</cite>.</p>
 
                 <h3><strong>Lenders</strong></h3>
                 <p>Dinosaur Designs, Doodad + Fandango, Jenny Kee, Jill Oliver, Klava Widdicombe, Linda Jackson, National Gallery of Australia, National Gallery of Victoria, Paul and Moira McKnight, Romance Was Born, Toni Walker-Lear</p>
--- src/pages/credits.js
+++ src/pages/credits.js
@@ -26,8 +26,8 @@ const CreditsPage = ({
               <h2>Before</h2>
               <h3>Video projection and photo animation</h3>
               <p>Design and animation by Anna Fraser and Julian Burgess (JULIANNA)</p>
-              <p>Photos courtesy Alamy, Chronicle, Cinesound Movietone, Edith Jackson, Fran Moore, George Lipman/The Sydney Morning Herald, Granger ― Historical Picture Archive, Herald & Weekly Times (UK) TopFoto, Jack Hickson, Marka, Mark Strizic, Monte Luke, Museum of Arts and Sciences, National Film and Sound Archive of Australia, Noel Herfort/The Sydney Morning Herald, Pat Woodley’s Modelling Agency, Roy Cushon/Roy Leighton Associates, State Library of New South Wales, State Library of Victoria, The Sunday Times/News Licensing, William Yang</p>
-              <h3>Trouble in Molopolis, 1970</h3>
+              <p>Photos courtesy Alamy, Chronicle, Cinesound Movietone, Edith Jackson, Fran Moore, George Lipman/<cite>The Sydney Morning Herald</cite>, Granger ― Historical Picture Archive, <cite>Herald & Weekly Times</cite> (UK) TopFoto, Jack Hickson, Marka, Mark Strizic, Monte Luke, Museum of Arts and Sciences, National Film and Sound Archive of Australia, Noel Herfort/<cite>The Sydney Morning Herald</cite>, Pat Woodley’s Modelling Agency, Roy Cushon/Roy Leighton Associates, State Library of New South Wales, State Library of Victoria, <cite>The Sunday Times</cite>/News Licensing, William Yang</p>
+              <h3><cite>Trouble in Molopolis</cite>, 1970</h3>
               <span>(7:41 min)</span>
               <p>Directed and produced by Philippe Mora. Courtesy National Film and Sound Archive of Australia</p>
               <p>Excerpts edited by Georgina Veneziani, Museum of Applied Arts and Sciences</p>
@@ -40,15 +40,15 @@ const CreditsPage = ({
               <h2>Screening Room</h2>
               <h3>Video projection</h3>
               <p>Art direction and production by Hank Mango (David Cochrane, John Grist and Michael Saad)</p>
-              <p>Photos courtesy Annie Noon, Bill Cunningham/The Bill Cunningham Foundation LLC, Bliss Swift, Brett Hilder, Brian Morris, Dominique Longheon, François Perez, Fran Moore, Grant Matthews, Jon Lewis, Kobya Panguana, Mike Molloy, Peter Muhlbock/Sunday Telegraph, Robert Rosen, Sandy Edwards, Tim Street-Porter, Wendy Adnam, William Yang</p>
+              <p>Photos courtesy Annie Noon, Bill Cunningham/The Bill Cunningham Foundation LLC, Bliss Swift, Brett Hilder, Brian Morris, Dominique Longheon, François Perez, Fran Moore, Grant Matthews, Jon Lewis, Kobya Panguana, Mike Molloy, Peter Muhlbock/<cite>Sunday Telegraph</cite>, Robert Rosen, Sandy Edwards, Tim Street-Porter, Wendy Adnam, William Yang</p>
 
               <h3>Films</h3>
               <p>ABC TV South East Forests protest footage courtesy Australian Broadcasting Corporation Library Sales</p>
               <p>Flamingo Follies 1975, 1977, 1980, 1981 excerpts produced by Swamplands Productions</p>
               <p>Flamingo Follies 1979 excerpts shown with kind permission from the estate of Clytie Jessop. Courtesy National Film and Sound Archive of Australia</p>
-              <p>The Mike Walsh Show excerpts courtesy Mike Walsh</p>
+              <p><cite>The Mike Walsh Show</cite> excerpts courtesy Mike Walsh</p>
               <p>Softly advertisement courtesy Pental</p>
-              <p>Good Morning Australia excerpt courtesy Network 10<img style={{width: '2em', marginLeft: '0.5em'}} src={tenLogo}/></p>
+              <p><cite>Good Morning Australia</cite> excerpt courtesy Network 10<img style={{width: '2em', marginLeft: '0.5em'}} src={tenLogo}/></p>
 
               <h2>Opals</h2>
               <p>Photos courtesy Fran Moore, Linda Jackson, Wendy Adnam</p>
