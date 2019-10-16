455b2a40c664b68b9170ed4756d1a3057dac9dec
lachlangordan <lachlangordan@PH09875.local>
2019-10-16 18:10:58 +1100

    add back in the meta viewport tag as that seemed to do something

--- src/html.js
+++ src/html.js
@@ -11,6 +11,9 @@ export default class HTML extends Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          { !shouldShowSeenIcon() && (
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          )}
 
           {/* Add custom css or scripts here */}
 
